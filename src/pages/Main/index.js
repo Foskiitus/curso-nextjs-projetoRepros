02f46a40e051df65459ba1c6060fad52fd5153df
Foskiitus <foskiitus@gmail.com>
2024-05-10 17:21:26 +0200

    Seccao 3 aula 19 styling Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,21 @@
-import { Title } from "./styles";
+import { Container, Form, SubmitButton } from "./styles";
+import { FaGithub, FaPlus } from "react-icons/fa";
+
 export default function Main() {
-  return <Title>Main</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithub size={25} />
+        Meus Repositorios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar Repositorios" />
+
+        <SubmitButton>
+          <FaPlus size={14} color="#FFFFFF" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
